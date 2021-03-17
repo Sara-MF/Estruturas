3f974ede857e7ee0352b31b72151a9131d95dfac
Sara Melo Frederico <38771339+Sara-MF@users.noreply.github.com>
2021-03-17 14:40:26 -0300

    Update Pilha.cpp

--- Pilha.cpp
+++ Pilha.cpp
@@ -1,7 +1,3 @@
-/* OBS: Todos as variáveis iniciadas com _ são referentes a própria pilha.
-   Quando há interação entre mais de uma pilha, é explicitado em
-   qual pilha está sendo feita determinada operação.                        */
-
 #include <iostream>
 using namespace std;
 
